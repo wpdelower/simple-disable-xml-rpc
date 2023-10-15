66016d3823bef8ff995ee6d354345ddb4edfac4c
wpdelower <delowerjes@gmail.com>
2023-10-15 16:21:13 +0600

    Uninstall.php file added

---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @package       SIMDISXMLRPC
+ * @author        Delower Hossain
+ * @license       gplv2
+ * @version       1.0
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+}
